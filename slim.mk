63001f3952286a6c91ffda544920dd76d69995fa
vocoderism <davide.franzino@gmail.com>
2014-06-25 14:36:43 +0200

    This is C-RoM

--- slim.mk
+++ slim.mk
@@ -5,18 +5,18 @@ PRODUCT_RELEASE_NAME := manta
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1600
 
-# Inherit some common SLIM stuff.
-$(call inherit-product, vendor/slim/config/common_full_tablet_wifionly.mk)
+# Inherit some common C-RoM stuff.
+$(call inherit-product, vendor/crom/config/common_full_tablet_wifionly.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/crom/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/manta/full_manta.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := manta
-PRODUCT_NAME := slim_manta
+PRODUCT_NAME := crom_manta
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 10
 PRODUCT_MANUFACTURER := samsung
